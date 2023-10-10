d4dfc594d8c0a26a8fda5457321d1ac28b00c549
mr-cheng111 <1959711225@qq.com>
2023-10-10 16:07:16 +0800

    Almost finish all bug but UART RX Can't get data.

--- include/lidar.hpp
+++ include/lidar.hpp
@@ -11,6 +11,31 @@
 #include <sensor_msgs/msg/laser_scan.h>
 #include <micro_ros_utilities/string_utilities.h>
 
+enum SYSTEM_FLAG
+{
+    SYSTEM_INIT = 0,
+    SYSTEM_NORMAL,
+    SYSTEM_STOP
+};
+
+typedef struct
+{   
+    uint32_t System_Status;
+    bool Wifi_Work_Flag;
+    bool ROS_Work_Flag;
+    bool Sensor_Work_Flag;
+    bool Serial_Work_Flag;
+    bool System_Time_Sync;
+
+}System_Status_t;
+
+typedef struct 
+{
+    String Wifi_SSID;
+    String Pass_Word;
+    String Host_Ip;
+    uint16_t Port;
+}WIFI_Data_t;
 
 using namespace std;
 
@@ -19,29 +44,45 @@ using namespace std;
 class lidar_t
 {
 public:
-    lidar_t(String Node_Name,
-            char* Wifi_SSID,
-            char* Pass_Word,
-            String Host_Ip,
-            uint16_t Port,
-            int Serial_Num);
+    HardwareSerial *Lidar_Serial;
+    HardwareSerial *System_Serial;
+    System_Status_t System_Status_Flag;
+private:
+    rclc_executor_t executor;
+    rclc_support_t support;
+    rcl_allocator_t allocator;
+    rcl_node_t pubnode;
 
-    lidar_t(uint8_t Serial_Num);
+    //声明话题发布者
+    rcl_publisher_t publisher;           // 声明话题发布者
+    sensor_msgs__msg__LaserScan pub_msg; // 声明消息文件
 
-    static void callback_subscription_(const void *msgin);
+    uint8_t Lidar_Rx_Buffer[58*4];
 
-    void Node_Work_Start(void)
-    {
-        delay(100);
-        rclc_executor_spin_some(&this->executor, RCL_MS_TO_NS(100));
-    }
+    const WIFI_Data_t Wifi_Data;
+
+public:
+    lidar_t(WIFI_Data_t Wifi_Input,int Serial_Num);
 
     void Micro_Ros_Task()
     {
+        
         digitalWrite(LED_Pin, HIGH);
+        this->pub_msg.header.frame_id = micro_ros_string_utilities_set(pub_msg.header.frame_id, "laser"); // 初始化消息内容
+        
+        //初始化雷达帧信息的递增角度、最远距离和最近距离。
+        this->pub_msg.angle_increment = 0.8 / 180 * PI;
+        this->pub_msg.range_min = 0.03;
+        this->pub_msg.range_max = 12.0;
         while(true)
         {
-            this->Node_Work_Start();
+            if (!rmw_uros_epoch_synchronized()) // 判断时间是否同步
+            {
+            rmw_uros_sync_session(1000); //  同步时间
+            continue;
+            }
+            this->System_Status_Flag.System_Time_Sync = true;
+            rclc_executor_spin_some(&this->executor, RCL_MS_TO_NS(1));
         }
     }
 
@@ -49,51 +90,37 @@ public:
     {
         while(true)
         {
-            if(!(this->Lidar_Serial->available() % 58))
+            if(this->System_Status_Flag.Wifi_Work_Flag)
             {
-                this->Lidar_Serial->read(this->Lidar_Rx_Buffer,58);
+                if(!(this->Lidar_Serial->available() % 58))
+                {
+                    this->System_Status_Flag.Sensor_Work_Flag = 1;
+                    this->Lidar_Serial->read(this->Lidar_Rx_Buffer,58);
+                }
+                rcl_publish(&publisher, &pub_msg, NULL);
             }
             delay(1);
         }
     }
+    void System_Monitor_Task()
+    {
+        while(true)
+        {
+            if(WiFi.status() != WL_CONNECTED)
+            {
+                this->System_Status_Flag.Wifi_Work_Flag = false;
+            }
+            // if();
+            delay(1);
+        }
+    }
 
-    void Ros_Init(String Node_Name);
+    void Ros_Init();
 
-    void Wifi_Init( char* Wifi_SSID,
-                    char* Pass_Word,
-                    String Host_Ip,
-                    uint16_t Port,
-                    uint16_t Wait_Time);
+    void Wifi_Init(const WIFI_Data_t Wifi_Input,const uint16_t Wait_Time);
                     
-    void Ros_Serial_Init(int Serial_Num);
+    void Ros_Serial_Init(const int Serial_Num);
 
     void Ros_Serial_Init(int Serial_Num,int Baud);
 
-    static void Usart_Callback_(void)
-    {
-        // if()
-
-
-
-
-    }
-public:
-    Ticker Tick;
-    HardwareSerial *Lidar_Serial;
-private:
-    rclc_executor_t executor;
-    rclc_support_t support;
-    rcl_allocator_t allocator;
-    rcl_node_t node;
-    // 声明话题订阅者
-    rcl_subscription_t subscriber;
-
-    //声明话题发布者
-    rcl_publisher_t publisher;           // 声明话题发布者
-    sensor_msgs__msg__LaserScan pub_msg; // 声明消息文件
-
-    // 声明消息文件
-    std_msgs__msg__Int32 sub_msg;
-    uint8_t Lidar_Rx_Buffer[58*4];
-    
 };
